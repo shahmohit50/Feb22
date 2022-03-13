69ceca790d4eaa1f3cc2d19d1f9fd982e00d25a5
shahmohit50 <shahmohit50@gmail.com>
2022-03-13 22:43:49 +0530

    Update MaxMinRecur.java

--- MaxMinRecur.java
+++ MaxMinRecur.java
@@ -2,13 +2,29 @@ package AssignTwo;
 
 public class MaxMinRecur {
 
+	static int arr [] = {1,-1,0,2,-2,3,-3,4,-4};
+	static int max =0;
+	static int min =0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int index =0;
+		Recur(index);
+		
 	}
 	
-	public static void Recur() {
-		
+	public static void Recur(int index) {
+		if(index >= arr.length) {
+			System.out.println("Max :"+max+ "  Min :"+min);
+			return;
+		}
+
+		if(arr[index] > max) {
+			max = arr[index];
+		}
+		if(arr[index] < min) {
+			min = arr[index];
+		}
+		Recur(index+1);
 	}
 
 }

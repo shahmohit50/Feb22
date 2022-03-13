143eeadd0417eca6fc876e017104d8cd4b27b1ef
shahmohit50 <shahmohit50@gmail.com>
2022-03-13 21:52:31 +0530

    Create MaxMinRecur.java

---
+++ MaxMinRecur.java
@@ -0,0 +1,14 @@
+package AssignTwo;
+
+public class MaxMinRecur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void Recur() {
+		
+	}
+
+}
